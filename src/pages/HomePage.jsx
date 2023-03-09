1da7972ce0bc7c0ffef84803620c88bb79b880c0
dhiran <dhiran2310@gmail.com>
2023-03-10 01:23:50 +0530

    Completed Login Page components and Initiated Home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,133 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Arrow from "../assets/Arrow_1.png";
+import "./HomePage.css"
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <Container>
+      <TopSection></TopSection>
+      <BottomSection>
+        <InfoSection>
+          <InfoContent>
+            <Circle>
+              <Number>1</Number>
+            </Circle>
+            <Text>Set Goal</Text>
+          </InfoContent>
+          <img src={Arrow} />
+          <InfoContent>
+            <Circle>
+              <Number>2</Number>
+            </Circle>
+            <Text>Update Daily Progress</Text>
+          </InfoContent>
+          <img src={Arrow} />
+          <InfoContent>
+            <Circle>
+              <Number>3</Number>
+            </Circle>
+            <Text>Track Progress</Text>
+          </InfoContent>
+          <img src={Arrow} />
+          <InfoContent>
+            <Circle>
+              <Number>4</Number>
+            </Circle>
+            <Text>Complete milestone</Text>
+          </InfoContent>
+        </InfoSection>
+        <ButtonSection></ButtonSection>
+      </BottomSection>
+    </Container>
+  );
+};
 
-export default HomePage
+const Container = styled.div`
+  width: 80vw;
+  height: 90vh;
+  background: black;
+  margin: auto;
+  margin-top: 40px;
+`;
+
+const TopSection = styled.div`
+  width: 100%;
+  height: 50%;
+`;
+
+const BottomSection = styled.div`
+  width: 100%;
+  height: 50%;
+`;
+
+const InfoSection = styled.div`
+  width: 100%;
+  height: 70%;
+  margin: auto;
+  display: flex;
+`;
+
+const ButtonSection = styled.div`
+  margin: auto;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+const InfoContent = styled.div`
+  position: relative;
+  top: 15%;
+  width: 210px;
+  height: 220px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+const Circle = styled.div`
+  width: 115px;
+  height: 115px;
+  border-radius: 50%;
+  background: #d9d9d9;
+  color: black;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Number = styled.span`
+  position: relative;
+  top: 18%;
+  left: 0;
+  font-family: "Inria Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 64px;
+  line-height: 77px;
+
+  color: #000000;
+`;
+
+const Text = styled.div`
+  position: relative;
+  font-family: "Inria Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 35px;
+  color: #ffffff;
+  width: 100%;
+  text-align: center;
+  margin: auto;
+`;
+
+const ArrowImg = styled.div`
+  background-image: url(../assets/Arrow_1.png);
+  width: 100px;
+  height: 100px;
+`;
+
+export default HomePage;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,94 @@
 import React from "react";
 import Login from "../components/Login";
+import styled from "styled-components";
+
 
 const LoginPage = () => {
   return (
-    <>
-      <Login />
-    </>
+    <Container>
+      <HeaderContent>
+        <Title>Project Pheonix</Title>
+        <SubTitle>“One Step, One punch, One Round at a time”</SubTitle>
+      </HeaderContent>
+      <LoginSection>
+        <Login />
+      </LoginSection>
+    </Container>
   );
 };
 
 export default LoginPage;
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`
+
+const HeaderContent = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  top: 10px;
+  margin: 15px;
+`;
+
+const Title = styled.h1`
+  font-family: "Inria Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 96px;
+  line-height: 115px;
+  background: linear-gradient(90deg, #ee0979 0%, #ff6a00 100%), #c1c1c1;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const SubTitle = styled.h3`
+  font-family: "Inria Sans";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 36px;
+  line-height: 43px;
+  margin-top: -60px;
+
+  background: linear-gradient(
+      90deg,
+      #8bdeda 0%,
+      #43add0 25%,
+      #998ee0 50%,
+      #e17dc2 75%,
+      #ef9393 100%
+    ),
+    linear-gradient(
+      90deg,
+      #ff75c3 0%,
+      #ffa647 20%,
+      #ffe83f 40%,
+      #9fff5b 60%,
+      #70e2ff 80%,
+      #cd93ff 100%
+    ),
+    linear-gradient(90deg, #d4fc79 0%, #96e6a1 100%),
+    linear-gradient(90deg, #2980b9 0%, #2c3e50 100%), #c1c1c1;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const LoginSection = styled.div`
+  position: relative;
+  left: 38%;
+  top: 20%;
+  width: 441px;
+  height: 500px;
+  align-items: center;
+  justify-content: center;
+  padding-top: 5px;
+  border-radius: 4px;
+`;
